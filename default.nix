06ecd814c7acc6f6a435e6ab52d3d65d7b563bca
Johannes Horner <lzy.info@pm.me>
2025-06-22 11:26:37 +0200

    fix currentSystem

--- default.nix
+++ default.nix
@@ -1,12 +1,24 @@
-{ system ? builtins.currentSystem
-, pkgs ? import ./pkgs { inherit system; }
-, callPackage ? pkgs.callPackage
-, package ? null
-, pname ? null
+{
+  system,
+  pkgs ? import ./pkgs { inherit system; },
+  callPackage ? pkgs.callPackage,
+  package ? null,
+  pname ? null,
 }:
 
-callPackage ({buildRacketPackage, buildThinRacket, lib, racket2nix}:
-if package == null then (racket2nix // pkgs)
-else if builtins.isString package then buildRacketPackage package
-else buildThinRacket ({ inherit package; } //
-  lib.optionalAttrs (builtins.isString pname) { inherit pname; })) {}
+callPackage (
+  {
+    buildRacketPackage,
+    buildThinRacket,
+    lib,
+    racket2nix,
+  }:
+  if package == null then
+    (racket2nix // pkgs)
+  else if builtins.isString package then
+    buildRacketPackage package
+  else
+    buildThinRacket (
+      { inherit package; } // lib.optionalAttrs (builtins.isString pname) { inherit pname; }
+    )
+) { }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,32 +1,55 @@
-{ pkgs ? import (import ../nixpkgs) {}
-, system ? builtins.currentSystem
-, racket-minimal ? pkgs.racket-minimal
-, ...
+{
+  pkgs ? import (import ../nixpkgs) { },
+  system,
+  racket-minimal ? pkgs.racket-minimal,
+  ...
 }@args:
 
 let
   nixpkgs = pkgs;
-  inherit (nixpkgs) bash lib newScope racket;
+  inherit (nixpkgs)
+    bash
+    lib
+    newScope
+    racket
+    ;
   inherit (lib) makeScope;
 in
 makeScope newScope (self: {
   pkgs = self;
-  callPackageFull = (makeScope self.newScope (fullself: nixpkgs // self //
-    { pkgs = fullself; extend = fullself.overrideScope'; })).callPackage;
+  callPackageFull =
+    (makeScope self.newScope (
+      fullself:
+      nixpkgs
+      // self
+      // {
+        pkgs = fullself;
+        extend = fullself.overrideScope';
+      }
+    )).callPackage;
   extend = self.overrideScope';
   racket-full = racket;
   inherit racket-minimal;
   racket = self.racket-minimal;
 
-  buildDrvs = name: buildInputs: derivation {
-    inherit name buildInputs system;
-    builder = bash + "/bin/bash";
-    args = [ "-c" "echo -n > $out" ];
-  };
-  racket2nix-stage0 = self.callPackage ../stage0.nix {};
-  racket2nix-stage1 = self.callPackage ../stage1.nix {};
+  buildDrvs =
+    name: buildInputs:
+    derivation {
+      inherit name buildInputs system;
+      builder = bash + "/bin/bash";
+      args = [
+        "-c"
+        "echo -n > $out"
+      ];
+    };
+  racket2nix-stage0 = self.callPackage ../stage0.nix { };
+  racket2nix-stage1 = self.callPackage ../stage1.nix { };
   racket2nix = self.buildRacketPackage "racket2nix";
-  inherit (self.callPackage ../build-racket.nix {})
-    buildRacket buildRacketPackage buildRacketCatalog
-    buildThinRacket buildThinRacketPackage;
+  inherit (self.callPackage ../build-racket.nix { })
+    buildRacket
+    buildRacketPackage
+    buildRacketCatalog
+    buildThinRacket
+    buildThinRacketPackage
+    ;
 })
